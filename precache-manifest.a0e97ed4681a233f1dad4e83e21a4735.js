2b7c2401834aa911ba6808d419933e2a5b05bc77
Ravindra Gore <ravindrashivn.gore@PROD.HCLPNP.COM>
2020-02-11 12:25:16 +0530

    Updates

---
+++ precache-manifest.a0e97ed4681a233f1dad4e83e21a4735.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1abe1860ec588ef1a02ee6c68ef83e4",
+    "url": "/reactlogin/index.html"
+  },
+  {
+    "revision": "a0d3e4dfe343c260384b",
+    "url": "/reactlogin/static/css/2.4eb5950e.chunk.css"
+  },
+  {
+    "revision": "b48d9510fc33bb3f162c",
+    "url": "/reactlogin/static/css/main.1f04f539.chunk.css"
+  },
+  {
+    "revision": "a0d3e4dfe343c260384b",
+    "url": "/reactlogin/static/js/2.50959792.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/reactlogin/static/js/2.50959792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b48d9510fc33bb3f162c",
+    "url": "/reactlogin/static/js/main.a8bc8f19.chunk.js"
+  },
+  {
+    "revision": "33016546d82bcba5e868",
+    "url": "/reactlogin/static/js/runtime-main.c80300d3.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/reactlogin/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/reactlogin/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/reactlogin/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/reactlogin/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/reactlogin/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
